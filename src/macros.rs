576947ad58da378dbf29a4175aeb7b75f6c254cf
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-15 11:40:57 -0400

    move macros to their own module and file

---
+++ src/macros.rs
@@ -0,0 +1,49 @@
+macro_rules! py_method {
+($name:ident, $ret:ty) => {
+    pub(crate) fn $name(&self) -> $ret {
+        Python::with_gil(|py| {
+            self.inner
+                .call_method1(py, stringify!($name), ())
+                .unwrap()
+                .extract(py)
+                .unwrap()
+        })
+    }
+};
+
+($name:ident, $ret:ty, $into:ident) => {
+    pub(crate) fn $name(&self) -> $ret {
+        let inner = Python::with_gil(|py| {
+            self.inner
+                .call_method1(py, stringify!($name), ())
+                .unwrap()
+                .into()
+        });
+        <$ret>::new(inner)
+    }
+};
+
+($name:ident, $ret:ty, $($arg:ident => $arg_typ:ty)*) => {
+    pub(crate) fn $name(&self, $($arg: $arg_typ)*) -> $ret {
+        Python::with_gil(|py| {
+            self.inner
+                .call_method1(py, stringify!($name), ($($arg,)*))
+                .unwrap()
+                .extract(py)
+                .unwrap()
+        })
+    }
+};
+
+($name:ident, $ret:ty, $($arg:ident => $arg_typ:ty)*, $into:ident) => {
+    pub(crate) fn $name(&self, $($arg: $arg_typ)*) -> $ret {
+        let inner = Python::with_gil(|py| {
+            self.inner
+                .call_method1(py, stringify!($name), ($($arg,)*))
+                .unwrap()
+                .into()
+        });
+        <$ret>::new(inner)
+    }
+};
+}
