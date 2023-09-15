8f118b466af34692b5483e46acd809a4925f8219
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-15 12:09:56 -0400

    delete build script

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-env-changed=CONDA_PREFIX");
-    let prefix = std::env::var("CONDA_PREFIX").unwrap();
-    println!("cargo:rustc-link-search={prefix}/lib");
-}
