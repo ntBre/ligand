1ba4b70ae650e412f93b6754b35ea6cb6a65d372
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-22 11:32:44 -0400

    add to_mapped_smiles, get_conformer, and add_conformer

--- src/molecule.rs
+++ src/molecule.rs
@@ -79,6 +79,36 @@ logging.getLogger("openff").setLevel(logging.ERROR)
         Ok(Self { inner })
     }
 
+    pub fn to_mapped_smiles(&self) -> String {
+        Python::with_gil(|py| {
+            let kwargs = [("mapped", true)].into_py_dict(py);
+            self.inner
+                .call_method(py, "to_smiles", (), Some(kwargs))
+                .unwrap()
+                .extract(py)
+                .unwrap()
+        })
+    }
+
+    /// return the Cartesian geometry of the `idx`th conformer of `self` as a
+    /// flattened vector in units of Å
+    pub fn get_conformer(&self, idx: usize) -> Vec<f64> {
+        Python::with_gil(|py| {
+            let fun = PyModule::from_code(
+                py,
+                "def get_conformer(mol, idx):
+    return mol.conformers[idx].magnitude.flatten().tolist()
+",
+                "",
+                "",
+            )
+            .unwrap()
+            .getattr("get_conformer")
+            .unwrap();
+            fun.call1((&self.inner, idx)).unwrap().extract().unwrap()
+        })
+    }
+
     pub fn to_svg(&self) -> String {
         Python::with_gil(|py| {
             let fun = PyModule::from_code(
@@ -102,6 +132,28 @@ logging.getLogger("openff").setLevel(logging.ERROR)
             .unwrap();
             fun.call1((self.inner.clone(),)).unwrap().extract().unwrap()
         })
+    }
+
+    pub fn add_conformer(&mut self, coordinates: Vec<f64>) {
+        Python::with_gil(|py| {
+            let fun = PyModule::from_code(
+                py,
+                "def add_conformer(mol, coordinates):
+    from openff.units import Quantity, unit
+    import numpy as np
+    a = np.array(coordinates)
+    a = np.reshape(a, (-1, 3))
+    c = Quantity(a, unit.angstrom)
+    mol.add_conformer(c)
+",
+                "",
+                "",
+            )
+            .unwrap()
+            .getattr("add_conformer")
+            .unwrap();
+            fun.call1((&self.inner, coordinates)).unwrap();
+        });
     }
 
     py_method! {to_inchi, String}
